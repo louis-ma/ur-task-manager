8df9c2142d8a7c5f900fd3a5c2e01eade0009a5e
desms11 <121095759+desms11@users.noreply.github.com>
2023-04-20 00:13:36 +0800

    working timer

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import { StrictMode } from "react";
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Timer from "./Timer";
+
+import { createRoot } from 'react-dom/client';
+
+createRoot(document.getElementById('root')).render(<Timer />);
+
+//timer component, uses TimerStyles.css, Timer.jsx
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <Timer />
+  </StrictMode>,
+  rootElement
+  ); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
